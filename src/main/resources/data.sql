c297ad4ff2db1d9c0f87328134dbc8f26341e627
crom0805 <crom0805@gmail.com>
2021-10-29 11:29:32 +0900

    CREATE 쿼리추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,27 @@ INSERT INTO AUTHORITY (AUTHORITY_NAME) values ('ROLE_ADMIN');
 
 INSERT INTO USER_AUTHORITY (SEQ, AUTHORITY_NAME) values (1, 'ROLE_USER');
 INSERT INTO USER_AUTHORITY (SEQ, AUTHORITY_NAME) values (1, 'ROLE_ADMIN');
+
+/*
+
+CREATE TABLE `authority` (
+  `authority_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`authority_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `user` (
+  `seq` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userid` varchar(50) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  `nickname` varchar(50) DEFAULT NULL,
+  `activated` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`seq`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `user_authority` (
+  `seq` bigint(20) NOT NULL,
+  `authority_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`seq`,`authority_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+*/
